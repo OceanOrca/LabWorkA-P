9ecd06aa2f7b67cab8f795d6a294075af881626e
Orca <maria.vorobyova192@gmail.com>
2017-09-13 22:35:10 +0300

    IDE Microsoft Visual Studio

---
+++ MyFirstProject/MyFirstProject/MyFirstProject.cpp
@@ -0,0 +1,15 @@
+// MyFirstProject.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int x;
+
+int main()
+{
+	cout << "Hello, World!\n";
+	cin >> x;
+    return 0;
+}
+
